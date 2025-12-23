b4507e54c6de03416f89431040015821431365fb
Gnister <sscharer@online.no>
2025-12-23 23:01:04 +0100

    nvim: more configurations

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,5 @@
 require("config.lazy")
+require("config.lsp")
 require("config.options")
 require("config.keymaps")
 require("config.autocmds")
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,9 @@
 return {
   'nvim-treesitter/nvim-treesitter',
-  lazy = false,
-  build = ':TSUpdate'
+  build = ':TSUpdate',
+  main = 'nvim-treesitter.config', -- This tells Lazy to look here for the .setup()
+  opts = {
+    ensure_installed = { 'help', 'lua', 'python', 'javascript', 'typescript', 'rust', 'go' },
+    highlight = { enable = true },
+  },
 }
