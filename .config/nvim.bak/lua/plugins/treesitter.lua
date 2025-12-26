cc6326ebe3af5b70a2a2b223944b8077909a6ea4
Gnister <sscharer@online.no>
2025-12-27 00:24:21 +0100

    update folder layout

--- .config/nvim.bak/lua/plugins/treesitter.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  main = 'nvim-treesitter.config', -- This tells Lazy to look here for the .setup()
-  opts = {
-    ensure_installed = { 'help', 'lua', 'python', 'javascript', 'typescript', 'rust', 'go' },
-    highlight = { enable = true },
-  },
-}
