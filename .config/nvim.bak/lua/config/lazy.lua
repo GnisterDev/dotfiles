4a6cf84e82d924d1c84ecd6c347ec274cf5058d8
Gnister <sscharer@online.no>
2025-12-26 16:20:21 +0100

    nvim config backup

--- .config/nvim.bak/lua/config/lazy.lua
+++ .config/nvim.bak/lua/config/lazy.lua
@@ -32,5 +32,5 @@ require("lazy").setup({
   install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
-  defaults = { lazy = false },
+  -- defaults = { lazy = false },
 })
